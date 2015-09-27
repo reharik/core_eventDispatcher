11cbc3bf5b70063afea88ff9449f8ed7eb2e9ab7
Raif Harik <reharik@gmail.com>
2015-09-27 11:42:53 -0500

    switched logic for buffer parser

--- src/bufferToJson.js
+++ src/bufferToJson.js
@@ -5,13 +5,14 @@
 
 module.exports = function(logger, JSON, buffer) {
     return function bufferToJson(item) {
-        if(!buffer.Buffer.isBuffer(item)){
-            logger.info('item is not a buffer, returning original item');
-            return item;
+        if(buffer.Buffer.isBuffer(item)){
+            logger.info('item is a buffer, parsing buffer');
+            console.log('buffer to parse')
+            console.log(item.toString())
+            return JSON.parse(item.toString('utf8'));
         }
-        logger.info('item is a buffer, parsing buffer');
-        console.log('buffer to parse')
-        console.log(item)
-        return JSON.parse(item.toString('utf8'));
+        logger.info('item is not a buffer, returning original item');
+        return item;
+
     }
 };
