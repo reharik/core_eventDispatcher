b593cbbea139a2c6fe3957284c2d55dc0137c593
Raif Harik <reharik@gmail.com>
2015-09-28 15:15:03 -0500

    oops

--- registry.js
+++ registry.js
@@ -9,8 +9,8 @@ module.exports = function(_options) {
     return new container(x=> x.pathToRoot(__dirname)
         .requireDirectoryRecursively('./src')
         .for('eventmodels').instantiate(i=>i.asFunc())
-        .for('eventstore').instantiate(i=>i.asFunc().withParameters(options.eventdispatcher || {}))
-        .for('eventDispatcher').instantiate(i=>i.asFunc().withParameters(options || {}))
+        .for('eventstore').instantiate(i=>i.asFunc().withParameters(options|| {}))
+        .for('eventDispatcher').instantiate(i=>i.asFunc().withParameters(options.eventdispatcher || {}))
         .for('bluebird').renameTo('Promise')
         .for('corelogger').renameTo('logger').instantiate(i=>i.asFunc().withParameters(options.logger || {}))
         .complete());
