8a2fc086c8f9dcc3737737017b8d8c5b756f7e5a
Raif Harik <reharik@gmail.com>
2015-09-28 15:11:48 -0500

    working on configs

--- registry.js
+++ registry.js
@@ -9,7 +9,8 @@ module.exports = function(_options) {
     return new container(x=> x.pathToRoot(__dirname)
         .requireDirectoryRecursively('./src')
         .for('eventmodels').instantiate(i=>i.asFunc())
-        .for('eventstore').instantiate(i=>i.asFunc().withParameters(options || {}))
+        .for('eventstore').instantiate(i=>i.asFunc().withParameters(options.eventdispatcher || {}))
+        .for('eventDispatcher').instantiate(i=>i.asFunc().withParameters(options || {}))
         .for('bluebird').renameTo('Promise')
         .for('corelogger').renameTo('logger').instantiate(i=>i.asFunc().withParameters(options.logger || {}))
         .complete());
