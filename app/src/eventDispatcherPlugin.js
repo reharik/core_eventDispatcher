709cf0d25d6c00652f1dff9ca33eb353efbadcf7
Raif Harik <reharik@gmail.com>
2016-01-09 14:19:55 -0600

    fixing plugin

--- app/src/eventDispatcherPlugin.js
+++ app/src/eventDispatcherPlugin.js
@@ -10,16 +10,19 @@ var eventDispatcher = function eventDispatcher(eventstore,
                                                mapAndFilterStream,
                                                serveToHandlers,treis) {
     return function(_handlers, streamType) {
-        logger.info('startDispatching | startDispatching called');
+        return {
+            startDispatching: function() {
+                logger.info('startDispatching | startDispatching called');
 
+                var mAndF  = mapAndFilterStream(streamType);
+                var _s     = serveToHandlers(_handlers);
+                var stream = rx.Observable.fromEvent(eventstore.subscribeToAllFrom(), 'event')
+                    .filter(mAndF.isValidStreamType)
+                    .map(mAndF.transformEvent);
 
-        var mAndF = mapAndFilterStream(streamType);
-        var _s = serveToHandlers(_handlers);
-        var stream = rx.Observable.fromEvent(eventstore.subscribeToAllFrom(), 'event')
-            .filter( mAndF.isValidStreamType)
-            .map( mAndF.transformEvent);
-
-        stream.subscribe(_s.serveEventToHandlers);
+                stream.subscribe(_s.serveEventToHandlers);
+            }
+        }
     };
 };
 module.exports = eventDispatcher;
